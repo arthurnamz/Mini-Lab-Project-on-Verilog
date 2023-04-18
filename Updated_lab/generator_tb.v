8846ec2c2cc9526d366f446d838de4a9458951cf
arthurnamz <arthurmwangonda@outlook.com>
2023-04-18 09:17:05 +0200

    Updating Generator Testbench

--- Updated_lab/generator_tb.v
+++ Updated_lab/generator_tb.v
@@ -1,66 +1,78 @@
 module generator_tb;
+    parameter DATA_SIZE = 32;
+    reg m00_axis_aclk;
+    reg m00_axis_aresetn;
+    reg m00_axis_enable;
+    wire  [DATA_SIZE-1:0]  m00_axis_tdata;
+    wire  [(DATA_SIZE/8)-1 : 0] m00_axis_tstrb;
+    wire   m00_axis_tvalid;
+    reg   m00_axis_tready;
+    wire  m00_axis_tlast;
 
-reg aclk = 0;
-reg aresetn;
-reg enable;
-wire [7:0] data_out;
-wire valid;
-reg ready;
+generator #(.DATA_SIZE(DATA_SIZE)) dut
+    (
+        .m00_axis_aclk(m00_axis_aclk), 
+        .m00_axis_aresetn(m00_axis_aresetn), 
+        .m00_axis_enable(m00_axis_enable), 
+        .m00_axis_tdata(m00_axis_tdata),
+        .m00_axis_tstrb(m00_axis_tstrb), 
+        .m00_axis_tvalid(m00_axis_tvalid), 
+        .m00_axis_tready(m00_axis_tready), 
+        .m00_axis_tlast(m00_axis_tlast)
+        );
 
-generator dut(.aclk(aclk), .aresetn(aresetn), .enable(enable), .data_out(data_out), .valid(valid), .ready(ready));
 
-
-    always #5 aclk = ~aclk;
+always #5 m00_axis_aclk = ~m00_axis_aclk;
 
 
 initial begin
     // reset the dut
-    aresetn = 0;
-    enable = 0;
-    ready = 0;
-    #20 aresetn = 1;
+    m00_axis_aresetn = 0;
+    m00_axis_enable = 0;
+    m00_axis_tready = 0;
+    #20 m00_axis_aresetn = 1;
 
     // wait for 5 clock cycles to ensure reset is complete
     #25;
 
     // start the generator
-    enable = 1;
-    ready = 1;
+    m00_axis_enable = 1;
+    m00_axis_tready = 1;
 
     // wait for 10 clock cycles to produce some output
     #50;
 
     // stop the generator
-    enable = 0;
-    ready = 0;
+    m00_axis_enable = 0;
+    m00_axis_tready = 0;
 
     // wait for 5 clock cycles
     #25;
 
     // start the generator again
-    enable = 1;
-    ready = 1;
+    m00_axis_enable = 1;
+    m00_axis_tready = 1;
 
     // wait for 10 clock cycles to produce some output
     #50;
 
     // stop the generator
-    enable = 0;
-    ready = 0;
+    m00_axis_enable = 0;
+    m00_axis_tready = 0;
 
     // wait for 5 clock cycles
     #25;
 
     // start the generator again
-    enable = 1;
-    ready = 1;
+    m00_axis_enable = 1;
+    m00_axis_tready = 1;
 
     // wait for 10 clock cycles to produce some output
     #50;
 
     // stop the generator
-    enable = 0;
-    ready = 0;
+    m00_axis_enable = 0;
+    m00_axis_tready = 0;
 
     // wait for 5 clock cycles
     #25;
@@ -68,9 +80,9 @@ initial begin
     $finish;
 end
 
-always @(posedge aclk) begin
-    if (valid && ready) begin
-        $display("data_out = %d", data_out);
+always @(posedge m00_axis_aclk) begin
+    if (m00_axis_tvalid && m00_axis_tready) begin
+        $display("m00_axis_tdata = %d", m00_axis_tdata);
     end
 end
 
