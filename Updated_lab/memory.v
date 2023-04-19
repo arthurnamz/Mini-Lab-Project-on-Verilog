3cbf5c89237574c62f1ad4085b10780d7a7634fe
arthurnamz <arthurmwangonda@outlook.com>
2023-04-19 14:39:18 +0200

    memory

--- Updated_lab/memory.v
+++ Updated_lab/memory.v
@@ -1,19 +1,30 @@
-module memory(
-    input clk,
-    input wr_en,
-    input valid,
-    input [7:0] wr_data,
-    input [7:0] wr_addr,
-    input rd_en,
-    input [7:0] rd_addr,
-    output ready,
-    output reg [7:0] rd_data
+module memory 
+#
+(
+    // Parameter declarations
+parameter MEM_SIZE = 4096, 
+parameter ADDR_WIDTH = 12, 
+parameter DATA_WIDTH = 32  
+)
+(
+    // input ports
+    input s01_axis_aclk,
+    input s01_axis_aresetn,
+    input s01_axis_wr_en,
+    input s01_axis_rd_en,
+    input s01_axis_tready,
+    input [ADDR_WIDTH-1:0] s01_axis_wr_addr,
+    input [ADDR_WIDTH-1:0] s01_axis_rd_addr,
+    input [DATA_WIDTH-1:0] s01_axis_wr_tdata,
+
+    // ouput port
+    output reg [DATA_WIDTH-1:0] s01_axis_rd_tdata,
+    output reg [(DATA_WIDTH/8)-1 : 0] s01_axis_tstrb,
+    output reg s01_axis_tvalid,
+    output reg s01_axis_tlast
 );
 
-// Parameter declarations
-parameter MEM_SIZE = 256; // 256 bytes of memory
-parameter ADDR_WIDTH = 8; // 8-bit address bus
-parameter DATA_WIDTH = 8; // 8-bit data bus
+
 
 // Internal signal declarations
 reg [DATA_WIDTH-1:0] mem[0:MEM_SIZE-1];
