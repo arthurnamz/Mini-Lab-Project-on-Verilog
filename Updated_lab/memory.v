f8d71e5aff4f7db4f8b15878b34133313d7bed75
arthurnamz <arthurmwangonda@outlook.com>
2023-04-19 19:16:49 +0200

    memory

--- Updated_lab/memory.v
+++ Updated_lab/memory.v
@@ -2,49 +2,48 @@ module memory
 #
 (
     // Parameter declarations
-//parameter MEM_SIZE = 4096, 
+parameter MEM_SIZE = 4096, 
 parameter ADDR_WIDTH = 12, 
 parameter DATA_WIDTH = 32  
 )
 (
-    // input ports
+    // slave input ports
     input s02_axis_aclk,
-    input m02_axis_aclk,
     input s02_axis_aresetn,
-    input m02_axis_aresetn,
-    input s02_axis_wr_en,
-    input m02_axis_rd_en,
-    input [ADDR_WIDTH-1:0] s02_axis_wr_addr,
-    input [ADDR_WIDTH-1:0] m02_axis_rd_addr,
     input [DATA_WIDTH-1:0] s02_axis_wr_tdata,
     input [(DATA_WIDTH/8)-1 : 0] s02_axis_tstrb,
     input s02_axis_tvalid,
     input s02_axis_tlast,
-    input m02_axis_tready,
+    output reg s02_axis_tready,
 
-    // ouput port
+    // master output port
+    input m02_axis_aclk,
+    input m02_axis_aresetn,
+    input m02_axis_tready,
     output reg [DATA_WIDTH-1:0] m02_axis_rd_tdata,
     output reg [(DATA_WIDTH/8)-1 : 0] m02_axis_tstrb,
     output reg m02_axis_tvalid,
-    output reg m02_axis_tlast,
-    output reg s02_axis_tready
-    
+    output reg m02_axis_tlast
 );
 
 
 
 // Internal signal declarations
-reg [DATA_WIDTH-1:0] mem[0:4095];
+reg [DATA_WIDTH-1:0] mem[0:MEM_SIZE-1];
+reg [ADDR_WIDTH-1:0] wr_addr_counter;
+reg [ADDR_WIDTH-1:0] rd_addr_counter;
 
 // Write operation
 always @(posedge s02_axis_aclk) begin
     if(~s02_axis_aresetn) begin
-        mem[s02_axis_wr_addr] <= 'bz;
-    end else if (s02_axis_wr_en && s02_axis_tvalid && s02_axis_tstrb && s02_axis_tlast) begin
-		mem[s02_axis_wr_addr] <= s02_axis_wr_tdata;
-        s02_axis_tready <= 'b1;
+        wr_addr_counter <= 12'b0;
+        s02_axis_tready <= 1'b0;
+    end else if (s02_axis_tvalid && s02_axis_tlast && s02_axis_tstrb) begin
+		mem[wr_addr_counter] <= s02_axis_wr_tdata;
+        wr_addr_counter <= wr_addr_counter + 1;
+        s02_axis_tready <= 1'b1;
     end else begin
-        mem[s02_axis_wr_addr] <= 'bz;
+        s02_axis_tready <= 1'b0;
     end
 end
 
@@ -52,14 +51,16 @@ end
 // Read operation
 always @(posedge m02_axis_aclk) begin
         if(~m02_axis_aresetn) begin
-            m02_axis_rd_tdata <= 'bz;
-        end else if (m02_axis_rd_en && m02_axis_tready ) begin
-            m02_axis_rd_tdata <= mem[m02_axis_rd_addr];
-            m02_axis_tvalid <= 'b1;
-            m02_axis_tstrb <= 'b1;
-            m02_axis_tlast <= 'b1;
+            rd_addr_counter =12'b0;
+            m02_axis_rd_tdata <= 1'bz;
+        end else if ( m02_axis_tready ) begin
+            m02_axis_rd_tdata <= mem[rd_addr_counter];
+            m02_axis_tvalid <= 1'b1;
+            m02_axis_tstrb <= 1'b1; 
+            m02_axis_tlast <= 1'b1;
+            rd_addr_counter <= rd_addr_counter + 1;
         end else begin
-            m02_axis_rd_tdata <= 'bz;
+            m02_axis_rd_tdata <= 1'bz;
         end
 end
 
