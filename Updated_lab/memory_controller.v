2a482b2626e0163fa7e23b5f5de79221049631c3
arthurnamz <arthurmwangonda@outlook.com>
2023-04-20 12:16:34 +0200

    memory wrapper

--- Updated_lab/memory_controller.v
+++ Updated_lab/memory_controller.v
@@ -1,76 +1,142 @@
 module memory_controller #(
-  // Parameter declarations
-  parameter MEM_SIZE = 4096,
-  parameter ADDR_WIDTH = 12,
-  parameter DATA_WIDTH = 32
-) (
-  // Slave input ports
-  input s01_axis_aclk,
-  input s01_axis_aresetn,
-  input [DATA_WIDTH-1:0] s01_axis_tdata,
-  input [(DATA_WIDTH/8)-1:0] s01_axis_tstrb,
-  input s01_axis_tvalid,
-  input s01_axis_tlast,
-  output reg s01_axis_tready,
-
-  // Master output ports
-  input m01_axis_aclk,
-  input m01_axis_aresetn,
-  input m01_axis_tready,
-  output reg [DATA_WIDTH-1:0] m01_axis_tdata,
-  output reg [(DATA_WIDTH/8)-1:0] m01_axis_tstrb,
-  output reg m01_axis_tvalid,
-  output reg m01_axis_tlast
+    // Parameter declarations
+    parameter MEM_SIZE = 4096, 
+    parameter ADDR_WIDTH = 12, 
+    parameter DATA_WIDTH = 32
+)
+(
+    // slave input ports
+    input s01_axis_aclk,
+    input s01_axis_aresetn,
+    input [DATA_WIDTH-1:0] s01_axis_tdata,
+    input [(DATA_WIDTH/8)-1:0] s01_axis_tstrb,
+    input s01_axis_tvalid,
+    input s01_axis_tlast,
+    output reg s01_axis_tready,
+    // master output port
+    input m01_axis_aclk,
+    input m01_axis_aresetn,
+    input m01_axis_tready,
+    output reg [DATA_WIDTH-1:0] m01_axis_tdata,
+    output reg [(DATA_WIDTH/8)-1:0] m01_axis_tstrb,
+    output reg m01_axis_tvalid,
+    output reg m01_axis_tlast
 );
 
-  // Internal registers 
-  reg [ADDR_WIDTH-1:0] wr_address_counter = 0;
-  reg [ADDR_WIDTH-1:0] rd_address_counter = 0;
-  reg [DATA_WIDTH-1:0] mem [0:MEM_SIZE-1];
-  reg [1:0] state;
+    // Internal signal declarations
+    reg [DATA_WIDTH-1:0] mem [0:MEM_SIZE-1];
+    reg [ADDR_WIDTH-1:0] wr_addr_counter;
+    reg [ADDR_WIDTH-1:0] rd_addr_counter;
 
-  // State machine states
-  localparam IDLE = 2'b00;
-  localparam WRITE = 2'b01;
-  localparam READ = 2'b10;
-
-  // State machine logic
-  always @(posedge s01_axis_aclk or posedge m01_axis_aclk) begin
-    if (~s01_axis_aresetn) begin
-      wr_address_counter <= 0;
-      s01_axis_tready <= 0;
-      state <= IDLE;
-    end else if (~m01_axis_aresetn) begin
-      rd_address_counter <= 0;
-      state <= IDLE;
-    end else begin
-      case (state)
-        IDLE: begin
-          if (s01_axis_tvalid && s01_axis_tstrb && s01_axis_tlast) begin
-            state <= WRITE;
-          end else if (m01_axis_tready) begin
-            state <= READ;
-          end
+    // Write operation
+    always @(posedge s01_axis_aclk) begin
+        if (~s01_axis_aresetn) begin
+            wr_addr_counter <= 0;
+            s01_axis_tready <= 0;
+        end else if (s01_axis_tvalid && s01_axis_tlast && s01_axis_tstrb) begin
+            mem[wr_addr_counter] <= s01_axis_tdata;
+            wr_addr_counter <= wr_addr_counter + 1;
+            s01_axis_tready <= 1;
+        end else begin
+            s01_axis_tready <= 0;
         end
-        WRITE: begin
-          mem[wr_address_counter] <= s01_axis_tdata;
-          wr_address_counter <= wr_address_counter + 1;
-          s01_axis_tready <= 1;
-          state <= IDLE;
-        end
-        READ: begin
-          m01_axis_tdata <= mem[rd_address_counter];
-          rd_address_counter <= rd_address_counter + 1;
-          m01_axis_tstrb <= 1;
-          m01_axis_tvalid <= 1;
-          m01_axis_tlast <= 1;
-          state <= IDLE;
-        end
-        default: begin
-          state <= IDLE;
-        end
-      endcase
     end
-  end
+
+    // Read operation
+    always @(posedge m01_axis_aclk) begin
+        if (~m01_axis_aresetn) begin
+            rd_addr_counter <= 0;
+            m01_axis_tdata <= 0;
+            m01_axis_tstrb <= 0;
+            m01_axis_tvalid <= 0;
+            m01_axis_tlast <= 0;
+        end else if (m01_axis_tready) begin
+            m01_axis_tdata <= mem[rd_addr_counter];
+            m01_axis_tstrb <= {(DATA_WIDTH/8){1'b1}};
+            m01_axis_tvalid <= 1'b1;
+            m01_axis_tlast <= (rd_addr_counter == (MEM_SIZE-1));
+            rd_addr_counter <= rd_addr_counter + 1;
+        end
+    end
+
 endmodule
+
+
+
+
+// module memory_controller #(
+//   // Parameter declarations
+//   parameter MEM_SIZE = 4096,
+//   parameter ADDR_WIDTH = 12,
+//   parameter DATA_WIDTH = 32
+// ) (
+//   // Slave input ports
+//   input s01_axis_aclk,
+//   input s01_axis_aresetn,
+//   input [DATA_WIDTH-1:0] s01_axis_tdata,
+//   input [(DATA_WIDTH/8)-1:0] s01_axis_tstrb,
+//   input s01_axis_tvalid,
+//   input s01_axis_tlast,
+//   output reg s01_axis_tready,
+
+//   // Master output ports
+//   input m01_axis_aclk,
+//   input m01_axis_aresetn,
+//   input m01_axis_tready,
+//   output reg [DATA_WIDTH-1:0] m01_axis_tdata,
+//   output reg [(DATA_WIDTH/8)-1:0] m01_axis_tstrb,
+//   output reg m01_axis_tvalid,
+//   output reg m01_axis_tlast
+// );
+
+//   // Internal registers 
+//   reg [ADDR_WIDTH-1:0] wr_address_counter = 0;
+//   reg [ADDR_WIDTH-1:0] rd_address_counter = 0;
+//   reg [DATA_WIDTH-1:0] mem [0:MEM_SIZE-1];
+//   reg [1:0] state;
+
+//   // State machine states
+//   localparam IDLE = 2'b00;
+//   localparam WRITE = 2'b01;
+//   localparam READ = 2'b10;
+
+//   // State machine logic
+//   always @(posedge s01_axis_aclk or posedge m01_axis_aclk) begin
+//     if (~s01_axis_aresetn) begin
+//       wr_address_counter <= 0;
+//       s01_axis_tready <= 0;
+//       state <= IDLE;
+//     end else if (~m01_axis_aresetn) begin
+//       rd_address_counter <= 0;
+//       state <= IDLE;
+//     end else begin
+//       case (state)
+//         IDLE: begin
+//           if (s01_axis_tvalid && s01_axis_tstrb && s01_axis_tlast) begin
+//             state <= WRITE;
+//           end else if (m01_axis_tready) begin
+//             state <= READ;
+//           end
+//         end
+//         WRITE: begin
+//           mem[wr_address_counter] <= s01_axis_tdata;
+//           wr_address_counter <= wr_address_counter + 1;
+//           s01_axis_tready <= 1;
+//           state <= IDLE;
+//         end
+//         READ: begin
+//           m01_axis_tdata <= mem[rd_address_counter];
+//           rd_address_counter <= rd_address_counter + 1;
+//           m01_axis_tstrb <= 1;
+//           m01_axis_tvalid <= 1;
+//           m01_axis_tlast <= 1;
+//           state <= IDLE;
+//         end
+//         default: begin
+//           state <= IDLE;
+//         end
+//       endcase
+//     end
+//   end
+// endmodule
 
--- Updated_lab/memory_controller_tb.v
+++ Updated_lab/memory_controller_tb.v
@@ -1,30 +1,35 @@
+
 `timescale 1ns/1ps
 
 module memory_controller_tb;
-    parameter PERIOD = 2; 
-    parameter MEM_SIZE = 4096; 
-    parameter ADDR_WIDTH = 12;
-    parameter DATA_WIDTH = 32; 
 
-    // slave input ports
-    reg s01_axis_aclk = 0;
+    // Parameters
+    parameter MEM_SIZE = 4096; 
+    parameter ADDR_WIDTH = 12; 
+    parameter DATA_WIDTH = 32;
+
+    // Signals
+    reg s01_axis_aclk;
     reg s01_axis_aresetn;
     reg [DATA_WIDTH-1:0] s01_axis_tdata;
-    reg [(DATA_WIDTH/8)-1 : 0] s01_axis_tstrb;
+    reg [(DATA_WIDTH/8)-1:0] s01_axis_tstrb;
     reg s01_axis_tvalid;
     reg s01_axis_tlast;
     wire s01_axis_tready;
-
-    // master output port
-    reg m01_axis_aclk = 0;
+    
+    reg m01_axis_aclk;
     reg m01_axis_aresetn;
     reg m01_axis_tready;
     wire [DATA_WIDTH-1:0] m01_axis_tdata;
-    wire [(DATA_WIDTH/8)-1 : 0] m01_axis_tstrb;
+    wire [(DATA_WIDTH/8)-1:0] m01_axis_tstrb;
     wire m01_axis_tvalid;
     wire m01_axis_tlast;
 
-    memory_controller #(.MEM_SIZE(MEM_SIZE), .ADDR_WIDTH(ADDR_WIDTH), .DATA_WIDTH(DATA_WIDTH)) dut(
+    // Instantiate the DUT
+    memory_controller dut (
+        .MEM_SIZE(MEM_SIZE),
+        .ADDR_WIDTH(ADDR_WIDTH),
+        .DATA_WIDTH(DATA_WIDTH),
         .s01_axis_aclk(s01_axis_aclk),
         .s01_axis_aresetn(s01_axis_aresetn),
         .s01_axis_tdata(s01_axis_tdata),
@@ -32,71 +37,171 @@ module memory_controller_tb;
         .s01_axis_tvalid(s01_axis_tvalid),
         .s01_axis_tlast(s01_axis_tlast),
         .s01_axis_tready(s01_axis_tready),
-
         .m01_axis_aclk(m01_axis_aclk),
         .m01_axis_aresetn(m01_axis_aresetn),
-        .m01_axis_tready(m01_axis_tready),
         .m01_axis_tdata(m01_axis_tdata),
         .m01_axis_tstrb(m01_axis_tstrb),
         .m01_axis_tvalid(m01_axis_tvalid),
-        .m01_axis_tlast(m01_axis_tlast)        
+        .m01_axis_tlast(m01_axis_tlast),
+        .m01_axis_tready(m01_axis_tready)
     );
 
-    // Clock generation 
-    always #(PERIOD/2) s01_axis_aclk = ~s01_axis_aclk;
-    always #(PERIOD/2) m01_axis_aclk = ~m01_axis_aclk;
+    // Clock generator
+    always #5 s01_axis_aclk = ~s01_axis_aclk;
+    always #10 m01_axis_aclk = ~m01_axis_aclk;
 
+    // Reset
     initial begin
-        s01_axis_aresetn = 1'b0;
-        m01_axis_aresetn = 1'b0;
-        #2;
-        s01_axis_aresetn = 1'b1;
-        m01_axis_aresetn = 1'b1;
-       
-
-        // test 1
-        #10;
-        // write data in the memory
-        s01_axis_tdata = 32'h0055;
-        s01_axis_tstrb = 1'b1;
-        s01_axis_tvalid = 1'b1;
-        s01_axis_tlast = 1'b1;
-
-        // test 2
-        #10;
-        // write data in the memory
-        s01_axis_tdata = 32'h0022;
-        s01_axis_tstrb = 1'b1;
-        s01_axis_tvalid = 1'b1;
-        s01_axis_tlast = 1'b1;
-
-        // test 3
-        #10;
-        // write data in the memory
-        s01_axis_tdata = 32'h0024;
-        s01_axis_tstrb = 1'b1;
-        s01_axis_tvalid = 1'b1;
-        s01_axis_tlast = 1'b1;
-
-        // // test 1 data out
-        #20;
-         // Read data from the memory
-        m01_axis_tready = 1'b1;
-
-        // // test 2 data out
-        #20;
-         // Read data from the memory
-        m01_axis_tready = 1'b1;
-
-        // // test 3 data out
-        #20;
-         // Read data from the memory
-        m01_axis_tready = 1'b1;
-
+        s01_axis_aclk = 0;
+        m01_axis_aclk = 0;
+        s01_axis_aresetn = 0;
+        m01_axis_aresetn = 0;
         #100;
-
-        $finish;
+        s01_axis_aresetn = 1;
+        m01_axis_aresetn = 1;
     end
 
+    // Write test
+    initial begin
+        // Wait for reset to complete
+        @(posedge s01_axis_aclk);
+        s01_axis_tvalid = 1;
+        s01_axis_tdata = 'h12345678;
+        s01_axis_tstrb = 4'b1111;
+        s01_axis_tlast = 1;
+        repeat(4) @(posedge s01_axis_aclk);
+        assert(s01_axis_tready == 1) else $error("Write operation failed: s01_axis_tready was not asserted.");
+        repeat(8) @(posedge s01_axis_aclk);
+        s01_axis_tvalid = 0;
+        s01_axis_tdata = 0;
+        s01_axis_tstrb = 0;
+        s01_axis_tlast = 0;
+    end
 
+    // Read test
+    initial begin
+        // Wait for write to complete
+        repeat(12) @(posedge s01_axis_aclk);
+        // Wait for reset to complete
+        // Start read operation
+    m01_axis_tready <= 1;
+    
+    // Wait for read to complete
+    repeat(4096) @(posedge m01_axis_aclk);
+    
+    // Verify the read data
+    integer i;
+    for (i = 0; i < 4096; i = i + 1) begin
+        if (mem[i] !== i) begin
+            $display("Read test failed at address %d", i);
+            $finish;
+        end
+    end
+    $display("Read test passed");
+    $finish;
+end
 endmodule
+
+
+// `timescale 1ns/1ps
+
+// module memory_controller_tb;
+//     parameter PERIOD = 2; 
+//     parameter MEM_SIZE = 4096; 
+//     parameter ADDR_WIDTH = 12;
+//     parameter DATA_WIDTH = 32; 
+
+//     // slave input ports
+//     reg s01_axis_aclk = 0;
+//     reg s01_axis_aresetn;
+//     reg [DATA_WIDTH-1:0] s01_axis_tdata;
+//     reg [(DATA_WIDTH/8)-1 : 0] s01_axis_tstrb;
+//     reg s01_axis_tvalid;
+//     reg s01_axis_tlast;
+//     wire s01_axis_tready;
+
+//     // master output port
+//     reg m01_axis_aclk = 0;
+//     reg m01_axis_aresetn;
+//     reg m01_axis_tready;
+//     wire [DATA_WIDTH-1:0] m01_axis_tdata;
+//     wire [(DATA_WIDTH/8)-1 : 0] m01_axis_tstrb;
+//     wire m01_axis_tvalid;
+//     wire m01_axis_tlast;
+
+//     memory_controller #(.MEM_SIZE(MEM_SIZE), .ADDR_WIDTH(ADDR_WIDTH), .DATA_WIDTH(DATA_WIDTH)) dut(
+//         .s01_axis_aclk(s01_axis_aclk),
+//         .s01_axis_aresetn(s01_axis_aresetn),
+//         .s01_axis_tdata(s01_axis_tdata),
+//         .s01_axis_tstrb(s01_axis_tstrb),
+//         .s01_axis_tvalid(s01_axis_tvalid),
+//         .s01_axis_tlast(s01_axis_tlast),
+//         .s01_axis_tready(s01_axis_tready),
+
+//         .m01_axis_aclk(m01_axis_aclk),
+//         .m01_axis_aresetn(m01_axis_aresetn),
+//         .m01_axis_tready(m01_axis_tready),
+//         .m01_axis_tdata(m01_axis_tdata),
+//         .m01_axis_tstrb(m01_axis_tstrb),
+//         .m01_axis_tvalid(m01_axis_tvalid),
+//         .m01_axis_tlast(m01_axis_tlast)        
+//     );
+
+//     // Clock generation 
+//     always #(PERIOD/2) s01_axis_aclk = ~s01_axis_aclk;
+//     always #(PERIOD/2) m01_axis_aclk = ~m01_axis_aclk;
+
+//     initial begin
+//         s01_axis_aresetn = 1'b0;
+//         m01_axis_aresetn = 1'b0;
+//         #2;
+//         s01_axis_aresetn = 1'b1;
+//         m01_axis_aresetn = 1'b1;
+       
+
+//         // test 1
+//         #10;
+//         // write data in the memory
+//         s01_axis_tdata = 32'h0055;
+//         s01_axis_tstrb = 1'b1;
+//         s01_axis_tvalid = 1'b1;
+//         s01_axis_tlast = 1'b1;
+
+//         // test 2
+//         #10;
+//         // write data in the memory
+//         s01_axis_tdata = 32'h0022;
+//         s01_axis_tstrb = 1'b1;
+//         s01_axis_tvalid = 1'b1;
+//         s01_axis_tlast = 1'b1;
+
+//         // test 3
+//         #10;
+//         // write data in the memory
+//         s01_axis_tdata = 32'h0024;
+//         s01_axis_tstrb = 1'b1;
+//         s01_axis_tvalid = 1'b1;
+//         s01_axis_tlast = 1'b1;
+
+//         // // test 1 data out
+//         #20;
+//          // Read data from the memory
+//         m01_axis_tready = 1'b1;
+
+//         // // test 2 data out
+//         #20;
+//          // Read data from the memory
+//         m01_axis_tready = 1'b1;
+
+//         // // test 3 data out
+//         #20;
+//          // Read data from the memory
+//         m01_axis_tready = 1'b1;
+
+//         #100;
+
+//         $finish;
+//     end
+
+
+// endmodule
